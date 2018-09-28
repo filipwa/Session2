0eb9f7caa8e9a06f85874027fbf785d4a78e7f14
filipwa <guswalfi@student.gu.se>
2018-09-28 12:16:24 +0200

    Add files via upload

---
+++ DateConverter.java
@@ -0,0 +1,43 @@
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Date;
+
+
+public class DateConverter {
+
+
+	// creates a Instant object of an XML time stamp
+	/**
+	 * @param instant
+	 * @return
+	 */
+	static public Instant parseDateTimestamp(String instant) {
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssX");
+		Date parsedDate = null;
+		;
+		try {
+			parsedDate = dateFormat.parse(instant);
+		} catch (ParseException e) {
+			System.out.println(e.toString());
+		}
+
+		return parsedDate.toInstant();
+	}
+
+	/**
+	 * 
+	 * @param instant
+	 * @param duration example: "PT2D"
+	 * @return
+	 */
+	public Instant addDurationToTimeStamp(String instant, String duration) {
+
+		return null;
+	}
+
+
+}
